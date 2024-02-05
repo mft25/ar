7d8898580d45aedce7f224cd19a13fba2f7e77db
Matthew Turner <matthewfturner@gmail.com>
2024-02-05 22:45:41 +0000

    Paste the sample HTML with some minor updates to have the duck display when the logo is detected - https://hiukim.github.io/mind-ar-js-doc/quick-start/assets#putting-it-together

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ import 'mind-ar/dist/mindar-image-aframe.prod.js';
 function App() {
   return (
     <>
-      <h1>MindAR Demo</h1>
+      <a-scene mindar-image="imageTargetSrc: ./targets.mind;" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false">
+        <a-assets>
+          <a-asset-item id="duckModel" src="./duck.glb"></a-asset-item>
+        </a-assets>
+
+        <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+
+        <a-entity mindar-image-target="targetIndex: 0">
+          <a-gltf-model rotation="0 -90 0 " position="0 0 0" scale="0.5 0.5 0.5" src="#duckModel" animation="property: position; to: 0 0.1 0.1; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate"></a-gltf-model>
+        </a-entity>
+      </a-scene>
     </>
   )
 }
